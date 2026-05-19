ea383fbb33f9d39e7579c3c4fb915db4e8be11ba
오창준 <cjbormk11@gmail.com>
2026-05-19 17:06:22 +0900

    feat: 헤더 UI 구현

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,11 @@
-import "./App.css";
+import Header from './components/Header'
 
 function App() {
-  return <></>;
+  return (
+    <div className="h-full min-h-screen max-w-full min-w-fit bg-gray-50">
+      <Header />
+    </div>
+  )
 }
 
-export default App;
+export default App
---
+++ web/src/components/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header className="fixed top-0 right-0 left-0 z-10 border-b border-gray-200 bg-white">
+      <div className="container mx-auto px-8 py-6">
+        <span className="text-2xl font-bold tracking-tight">
+          Coloring Book Generator
+        </span>
+      </div>
+    </header>
+  )
+}
+
+export default Header
